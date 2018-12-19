713ebce94d80812af634f0e503e3729713f9bae9
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2018-12-19 23:44:07 +0300

    Add activeadmin

---
+++ db/migrate/20181219204258_create_excursions.rb
@@ -0,0 +1,6 @@
+class CreateExcursions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :excursions do |t|
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'iam@mail.com', password: 'passpass', password_confirmation: 'passpass') if Rails.env.development?
