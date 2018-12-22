32c10b02e5a186f3ae42846b976da8d5249bdd63
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2018-12-22 23:49:49 +0300

    Add nice date input

--- Gemfile
+++ Gemfile
@@ -67,4 +67,6 @@ gem 'devise'
 
 gem 'activeadmin_froala_editor'
 
+gem 'active_admin_datetimepicker'
+
 gem 'formadmin'
--- app/admin/excursions.rb
+++ app/admin/excursions.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register Excursion do
   permit_params :name, :start_date, :short_description, :description, :guide_id
 
-  filter :name
+  filter :start_date, as: :date_time_range
 
   form do |f|
     f.inputs do
       f.input :name
-      f.input :start_date
+      f.input :start_date, :as :date_time_range,  datepicker_options: { min_date: Data.today,        max_date: "+3D" }
       f.input :short_description
       f.input :description, :as => :froala_editor
       f.input :guide_id, as: :select, :collection => Participant.all.map{|t| [t.full_name, t.id]}
