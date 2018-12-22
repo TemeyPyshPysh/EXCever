ef37acb1c666ac206e0f0fee9aba5962d6ed6627
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2018-12-22 22:51:03 +0300

    Update excursion model

--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -12,7 +12,7 @@ class Excursion < ApplicationRecord
 
   def short_discription_is_not_empty_and_its_length_less_40
     if not short_description.present?
-      erros.add(:short_description, "can't be empty")
+      errors.add(:short_description, "can't be empty")
     end
     if short_description.present? && (short_description == '' || short_description.length > 40)
       errors.add(:short_description, "can't be empty and more than 40 characters")
@@ -21,7 +21,7 @@ class Excursion < ApplicationRecord
 
   def name_is_not_empty
     if not name.present?
-      erros.add(:name, "can't be empty")
+      errors.add(:name, "can't be empty")
     end
     if name.present? && name == ''
       errors.add(:name, "can't be empty")
@@ -30,7 +30,7 @@ class Excursion < ApplicationRecord
 
   def start_date_cannot_be_in_the_past
     if not start_date.present?
-      erros.add(:start_date, "can't be empty")
+      errors.add(:start_date, "can't be empty")
     end
     if start_date.present? && start_date < Date.today
       errors.add(:start_date, "can't be in the past")
