5936d8ec0ef8b534f037ccef786bbd8eee1b0144
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2018-12-22 21:02:15 +0300

    Update registration of excursion

--- app/admin/excursions.rb
+++ app/admin/excursions.rb
@@ -9,7 +9,8 @@ ActiveAdmin.register Excursion do
       f.input :start_date
       f.input :short_description
       f.input :description, :as => :froala_editor
-      f.input :guide_id, as: :select, :collection => Participant.all.map{|t| [t.id]}
+    f.inputs do
+      f.input :guide_id, as: :select, :collection => Participant.all.map{|t| [t.full_name, t.id]}
     end
     f.actions
   end
