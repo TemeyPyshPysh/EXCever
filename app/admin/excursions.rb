42e5b3bc1039e44dfc182f3a5325ec48d2abeadc
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2018-12-23 00:11:26 +0300

    Add activeadmin addons

--- app/admin/excursions.rb
+++ app/admin/excursions.rb
@@ -6,9 +6,10 @@ ActiveAdmin.register Excursion do
   form do |f|
     f.inputs do
       f.input :name
-      f.input :start_date
+      f.input :start_date, as: :date_time_picker
       f.input :short_description
       f.input :description, :as => :froala_editor
+      f.input :color, as: :color_picker
       f.input :guide_id, as: :select, :collection => Participant.all.map{|t| [t.full_name, t.id]}
     end
     f.actions
