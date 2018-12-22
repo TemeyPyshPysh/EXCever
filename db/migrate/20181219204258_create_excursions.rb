a3b64d3b5968e08001c1c8a6afffab2166295a57
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2018-12-22 18:21:22 +0300

    Update db migrates

--- db/migrate/20181219204258_create_excursions.rb
+++ db/migrate/20181219204258_create_excursions.rb
@@ -1,19 +1,19 @@
 class CreateExcursions < ActiveRecord::Migration[5.2]
   def change
-    drop_table :users
-    create_table :users do |t|
+    # drop_table :users
+    create_table :participants do |t|
       t.string :full_name
       t.string :nickname
       t.integer :rating
       t.timestamps
     end
-    drop_table :users
+    # drop_table :users
     create_table :excursions do |t|
       t.string :name
       t.date :start_date
       t.text :description
       t.text :short_description
-      t.belongs_to :user
+      t.belongs_to :participant
       t.timestamps
     end
   end
