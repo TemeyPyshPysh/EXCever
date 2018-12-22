a88884c598017dd837d436b1816849528cca5221
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2018-12-22 23:02:51 +0300

    Update participant model

--- app/admin/excursions.rb
+++ app/admin/excursions.rb
@@ -9,7 +9,8 @@ ActiveAdmin.register Excursion do
       f.input :start_date
       f.input :short_description
       f.input :description, :as => :froala_editor
-      f.input :guide_id, as: :select, :collection => Participant.all.map{|t| [t.full_name, t.id]}
+      f.input :guide_id, as: :select, :collection => Participant.all.map{|t| [t.full_name, "%s %s"%[t.full_name,
+                                                                                                    t.nickname]]}
     end
     f.actions
   end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,25 @@
 class Participant < ApplicationRecord
+  validate :not_empty_full_name
+  validate :not_empty_uniq_nickname
+
+  def not_empty_uniq_nickname
+    if not nickname.present?
+      errors.add(:nickname, "can't be empty")
+    end
+    if Participant.all.map{|t| t.nickname}.include? nickname
+      erros.add(:nickname, "must be unique!")
+    end
+  end
+
+  def not_empty_full_name
+    if not full_name.present?
+      errors.add(:full_name, "can't be empty")
+    end
+    if full_name.length > 40
+      erros.add(:full_name, "can't be more than 40 characters")
+    end
+  end
+
   has_many :excursions, :through => :tripper_excurs
   has_many :excursions
 end
