e0db73aa26aeed5dc78fbd0c6c88b18f42604355
TemeyPyshPysh <iwahnencko1996@yandex.ru>
2018-12-22 21:49:43 +0300

    Update registration of excursion

--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -1,11 +1,4 @@
 class Excursion < ApplicationRecord
-  validate :start_date_cannot_be_in_the_past
-
-  def expiration_date_cannot_be_in_the_past
-    if start_date.present? && start_date < Date.today
-      errors.add(:start_date, "can't be in the past")
-    end
-  end
 
   has_many :participants, :through => :tripper_excurss
   has_one :participant
